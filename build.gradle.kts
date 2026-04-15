04732e85d99810d8a2d937a4717136cf61d55f9d
minseo <p7548296@gmail.com>
2026-04-15 22:48:51 +0900

    14강, SystemControllerTest, WiseSayingControllerTest 도입, 패키지 나누기

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,20 @@ repositories {
     mavenCentral()
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.27.3")
+}
+
+tasks.withType<org.gradle.api.tasks.compile.JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
 }
 
 tasks.test {
---
+++ src/test/java/com/ll/domain/system/SystemControllerTest.java
@@ -0,0 +1,4 @@
+package com.ll.domain.system;
+
+public class SystemControllerTest {
+}
