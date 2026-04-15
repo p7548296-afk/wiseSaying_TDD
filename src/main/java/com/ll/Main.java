5e496c7a002f47fecd0db748ee6125ba4f0930ef
minseo <p7548296@gmail.com>
2026-04-15 23:20:23 +0900

    16 - 스텝 08, blue 06

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -8,8 +8,6 @@ public class Main {
     public static void main(String[] args) {
         AppContext.scanner = new Scanner(System.in);
         new App().run();
-//        lab1();
-//        lab2();
     }
 
     public static void lab1 () {
