2272c324547a1a5fbb04c26d366ba8afca77234d
minseo <p7548296@gmail.com>
2026-04-15 23:19:18 +0900

    16

--- src/main/java/com/ll/AppTestRunner.java
+++ src/main/java/com/ll/AppTestRunner.java
@@ -1,4 +1,19 @@
 package com.ll;
 
+import com.ll.standard.util.TestUtil;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
 public class AppTestRunner {
+    public static String run(String input) {
+        Scanner scanner = TestUtil.genScanner(input + "\n종료");
+
+        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
+
+        AppContext.scanner = scanner;
+        new App().run();
+
+        return output.toString();
+    }
 }
