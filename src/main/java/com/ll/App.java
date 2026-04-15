0b76367e724c17ea8d26540d97ee0808e9d4e775
minseo <p7548296@gmail.com>
2026-04-15 20:07:14 +0900

    9강 ~ 11강 까지 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -10,6 +10,31 @@ public class App {
     }
 
     public void run () {
+        int lastId = 0;
         System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.println("명령) ");
+            String cmd = scanner.nextLine();
+
+            switch (cmd) {
+                case "등록" -> {
+                    System.out.println("명언 : ");
+                    String content = scanner.nextLine();
+
+                    System.out.println("작가 : ");
+                    String author = scanner.nextLine();
+
+                    int id = ++lastId;
+
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                }
+                case "종료" -> {
+                    return;
+                }
+            }
+
+        }
+
     }
 }
--- src/test/java/com/ll/AppTest.java
+++ src/test/java/com/ll/AppTest.java
@@ -10,7 +10,52 @@ public class AppTest {
     @Test
     @DisplayName("`== 명언 앱 == ` 출력")
     public void t1 () {
-        String rs = AppTestRunner.run("종료");
-        assertThat(rs).contains("== 명언 앱 ==");
+        String rs = AppTestRunner.run("");
+        assertThat(rs)
+                .contains("== 명언 앱 ==")
+                .contains("명령) ");
+    }
+
+    @Test
+    @DisplayName("등록")
+    void t2() {
+        String rs = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        assertThat(rs)
+                .contains("명언 : ")
+                .contains("작가 : ");
+    }
+
+    @Test
+    @DisplayName("등록시 생성된 명언번호 노출")
+    void t3() {
+        String rs= AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        assertThat(rs).contains("1번 명언이 등록되었습니다.");
+    }
+
+    @Test
+    @DisplayName("등록할때 마다 생성되는 명언번호가 증가")
+    void t4() {
+        String rs = AppTestRunner.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        assertThat(rs)
+                .contains("1번 명언이 등록되었습니다.")
+                .contains("2번 명언이 등록되었습니다.");
     }
 }
